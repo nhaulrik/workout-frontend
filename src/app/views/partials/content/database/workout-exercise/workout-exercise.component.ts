06e4e939ccac23b5a52e568b8086955d0bfa0b6f
nch <nikolajmx@hotmail.com>
2020-11-21 12:33:02 +0100

    restructure (wip)

--- src/app/views/partials/content/database/workout-exercise/workout-exercise.component.ts
+++ src/app/views/partials/content/database/workout-exercise/workout-exercise.component.ts
@@ -2,7 +2,7 @@ import {Component, ComponentFactoryResolver, ComponentRef, OnInit, ViewChild, Vi
 import {Exercise} from '../../../../../core/database/_models/exercise';
 import {ExerciseService} from '../../../../../core/database/_services/exercise.service';
 import {GraphQlResponse} from '../../../../../core/database/_models/graphQlResponse';
-import {myinterface, WorkoutSetComponent} from '../workout-set/workout-set.component';
+import {WorkoutSetComponent} from '../workout-set/workout-set.component';
 import {SessionComponent} from '../session/session.component';
 
 @Component({
@@ -35,45 +35,6 @@ export class WorkoutExerciseComponent implements OnInit, myinterface {
 		this.getExercises();
 	}
 
-	createComponent() {
-		let componentFactory = this.CFR.resolveComponentFactory(WorkoutSetComponent);
-
-		let childComponentRef = this.VCR.createComponent(componentFactory);
-
-		let childComponent = childComponentRef.instance;
-		childComponent.unique_key = ++this.child_unique_key;
-		childComponent.parentRef = this;
-
-		childComponent.setNumber = this.componentsReferences.length + 1;
-		// add reference for newly created component
-		this.componentsReferences.push(childComponentRef);
-	}
-
-	remove(key: number) {
-		if (this.VCR.length < 1) return;
-
-		let componentRef = this.componentsReferences.filter(
-			x => x.instance.unique_key == key
-		)[0];
-
-		let vcrIndex: number = this.VCR.indexOf(componentRef as any);
-
-		// removing component from container
-		this.VCR.remove(vcrIndex);
-
-		// removing component from the list
-		this.componentsReferences = this.componentsReferences.filter(
-			x => x.instance.unique_key !== key
-		);
-	}
-
-	getWorkoutSetAmount(number: number) {
-		if (number == undefined) {
-			return [];
-		}
-		return Array(number).fill(0).map((x, i) => i); // [0,1,2,3,4]
-	}
-
 	getExercises() {
 		this.exerciseService.getExercises()
 			.subscribe(response => {
@@ -92,30 +53,70 @@ export class WorkoutExerciseComponent implements OnInit, myinterface {
 		this.workoutSetMap.set(exerciseId, currentAmountOfWorkoutSet + 1);
 	}
 
-	updateExerciseId(exerciseIndex: number, $event: string) {
-		let exerciseId = $event;
-		let currentAmountOfWorkoutSet = this.workoutSetMap.get(exerciseId);
+	// getWorkoutSetAmount(number: number) {
+	// 	if (number == undefined) {
+	// 		return [];
+	// 	}
+	// 	return Array(number).fill(0).map((x, i) => i); // [0,1,2,3,4]
+	// }
 
-		let currentExerciseId = this.sessionExercises.get(exerciseIndex)
-
-		if (currentAmountOfWorkoutSet == undefined && currentExerciseId == undefined) {
-			currentAmountOfWorkoutSet = 0;
-		} else {
-			currentAmountOfWorkoutSet = this.workoutSetMap.get(currentExerciseId);
-		}
-
-		this.sessionExercises.set(exerciseIndex, exerciseId);
-		this.workoutSetMap.set(exerciseId, currentAmountOfWorkoutSet);
-
-	}
+	// updateExerciseId(exerciseIndex: number, $event: string) {
+	// 	let exerciseId = $event;
+	// 	let currentAmountOfWorkoutSet = this.workoutSetMap.get(exerciseId);
+	//
+	// 	let currentExerciseId = this.sessionExercises.get(exerciseIndex)
+	//
+	// 	if (currentAmountOfWorkoutSet == undefined && currentExerciseId == undefined) {
+	// 		currentAmountOfWorkoutSet = 0;
+	// 	} else {
+	// 		currentAmountOfWorkoutSet = this.workoutSetMap.get(currentExerciseId);
+	// 	}
+	//
+	// 	this.sessionExercises.set(exerciseIndex, exerciseId);
+	// 	this.workoutSetMap.set(exerciseId, currentAmountOfWorkoutSet);
+	//
+	// }
 
 	removeWorkoutExercise() {
 		console.log(this.unique_key)
 		this.parentRef.remove(this.unique_key)
 	}
+
+	createWorkoutSetComponent() {
+		let componentFactory = this.CFR.resolveComponentFactory(WorkoutSetComponent);
+
+		let childComponentRef = this.VCR.createComponent(componentFactory);
+
+		let childComponent = childComponentRef.instance;
+		childComponent.unique_key = ++this.child_unique_key;
+		childComponent.parentRef = this;
+
+		childComponent.setNumber = this.componentsReferences.length + 1;
+		// add reference for newly created component
+		this.componentsReferences.push(childComponentRef);
+	}
+
+	removeWorkoutSetComponent(key: number) {
+		if (this.VCR.length < 1) return;
+
+		let componentRef = this.componentsReferences.filter(
+			x => x.instance.unique_key == key
+		)[0];
+
+		let vcrIndex: number = this.VCR.indexOf(componentRef as any);
+
+		// removing component from container
+		this.VCR.remove(vcrIndex);
+
+		// removing component from the list
+		this.componentsReferences = this.componentsReferences.filter(
+			x => x.instance.unique_key !== key
+		);
+	}
+
 }
 
 // Interface
 export interface myinterface {
-	remove(index: number);
+	removeWorkoutSetComponent(index: number);
 }
