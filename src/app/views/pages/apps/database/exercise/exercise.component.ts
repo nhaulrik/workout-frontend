25412e00eb6c2d27d2c4c4d45eb566749a3bd8a8
nch <nikolajmx@hotmail.com>
2020-12-12 22:17:44 +0100

    load exercises onInit

--- src/app/views/pages/apps/database/exercise/exercise.component.ts
+++ src/app/views/pages/apps/database/exercise/exercise.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ExerciseService} from '../../../../../core/database/_services/exercise.service';
+import {Exercise} from '../../../../../core/database/_models/exercise';
+import {GraphQlResponse} from '../../../../../core/database/_models/graphQlResponse';
 
 @Component({
-  selector: 'kt-exercise',
-  templateUrl: './exercise.component.html',
-  styleUrls: ['./exercise.component.scss']
+	selector: 'kt-exercise',
+	templateUrl: './exercise.component.html',
+	styleUrls: ['./exercise.component.scss'],
+	providers: [ExerciseService],
 })
 export class ExerciseComponent implements OnInit {
+	exercises: Exercise[] = [];
 
-  constructor() { }
+	constructor(
+		private exerciseService: ExerciseService,
+	) {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.exerciseService.getExercises().subscribe(response => {
+			let data = (response as GraphQlResponse).data;
+			this.exercises = data.exercises;
+		})
+	}
 
 }
