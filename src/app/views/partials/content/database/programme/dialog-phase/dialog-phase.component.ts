2c6cad1e0e5828245c8e8c117cdb6b5b598bae8f
nch <nikolajmx@hotmail.com>
2021-02-16 17:20:04 +0100

    load exercises to display in split

--- src/app/views/partials/content/database/programme/dialog-phase/dialog-phase.component.ts
+++ src/app/views/partials/content/database/programme/dialog-phase/dialog-phase.component.ts
@@ -13,13 +13,19 @@ import {MAT_DIALOG_DATA, MatDialogRef, MatSnackBar} from '@angular/material';
 import {Phase} from '../../../../../../core/database/_models/programme/phase';
 import {SplitComponent} from '../split/split.component';
 import {Split} from '../../../../../../core/database/_models/programme/split';
+import {Exercise} from '../../../../../../core/database/_models/exercise';
+import {ExerciseService} from '../../../../../../core/database/_services/exercise.service';
+import {GraphQlResponse} from '../../../../../../core/database/_models/graphQlResponse';
 
 @Component({
 	selector: 'kt-dialog-phase',
 	templateUrl: './dialog-phase.component.html',
-	styleUrls: ['./dialog-phase.component.scss']
+	styleUrls: ['./dialog-phase.component.scss'],
+	providers: [ExerciseService]
 })
 export class DialogPhaseComponent implements OnInit, AfterViewInit {
+
+	exercises: Exercise[] = [];
 
 	phase: Phase = {
 		name: null,
@@ -39,6 +45,7 @@ export class DialogPhaseComponent implements OnInit, AfterViewInit {
 		public dialogRef: MatDialogRef<DialogPhaseComponent>,
 		@Inject(MAT_DIALOG_DATA) public data: any,
 		public snackBar: MatSnackBar,
+		private exerciseService: ExerciseService,
 	) {
 	}
 
@@ -47,7 +54,10 @@ export class DialogPhaseComponent implements OnInit, AfterViewInit {
 	}
 
 	ngAfterViewInit(): void {
-		this.createSplitComponents(this.phase.splits)
+		this.exerciseService.getExercises().subscribe(response => {
+			this.exercises = (response as GraphQlResponse).data.exercises.sort((ex1, ex2) => ex1.name.localeCompare(ex2.name));
+			this.createSplitComponents(this.phase.splits);
+		});
 	}
 
 	private createSplitComponents(splits: Split[]) {
@@ -70,6 +80,7 @@ export class DialogPhaseComponent implements OnInit, AfterViewInit {
 			childComponent.parentRef = this;
 
 			childComponent.split = split;
+			childComponent.exerciseDictionary = this.exercises;
 
 			// add reference for newly created component
 			this.splitReferences.push(childComponentRef);
--- src/app/views/partials/content/database/programme/split/split.component.ts
+++ src/app/views/partials/content/database/programme/split/split.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DialogPhaseComponent} from '../dialog-phase/dialog-phase.component';
 import {Split} from '../../../../../../core/database/_models/programme/split';
+import {Exercise} from '../../../../../../core/database/_models/exercise';
 
 @Component({
 	selector: 'kt-split',
@@ -12,7 +13,15 @@ export class SplitComponent implements OnInit {
 	public unique_key: number;
 	public parentRef: DialogPhaseComponent;
 
-	split: Split;
+	split: Split = {
+		number: null,
+		dayOfWeek: null,
+		id: null,
+		name: null,
+		phaseId: null,
+		week: null
+	};
+	exerciseDictionary: Exercise[] = [];
 
 	constructor() {
 	}
